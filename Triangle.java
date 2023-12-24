ec64fdc9dd340537bae7b9d085d99637b6bd9939
Lavie-Zanzuri <154608667+Lavie-Zanzuri@users.noreply.github.com>
2023-12-24 19:56:00 +0200

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -8,7 +8,7 @@ public class Triangle {
 		int a = Integer.parseInt(args[0]);
 		int b = Integer.parseInt(args[1]);
 		int c = Integer.parseInt(args[2]);
-		if (a+b>c) {
+		if (a + b > c) {
 			System.out.println(a+", "+b+", "+c+": true");
 		}
 		else {
