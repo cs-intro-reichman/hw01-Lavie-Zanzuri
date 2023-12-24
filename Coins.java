37d716355372ffd05458da48bc5fcfbf8385eded
Lavie-Zanzuri <154608667+Lavie-Zanzuri@users.noreply.github.com>
2023-12-24 14:44:19 +0200

    Update Coins.java

--- Coins.java
+++ Coins.java
@@ -4,6 +4,10 @@
  */
 public class Coins {
 	public static void main(String[] args) {
-		// Put your code here
+		int a=Integer.parseInt(args[0]);
+		int cent,quarter;
+		quarter = a / 25;
+		cent = a % 25;
+		System.out.println("Use "+quarter+" quarters"+" and "+cent+" cents");	
 	}
 }
